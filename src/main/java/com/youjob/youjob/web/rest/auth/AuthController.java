bdaedf05ffa814da3f8d1fe40706b6046b1524cf
OussamaBL <oussamaazrour@gmail.com>
2025-01-19 18:34:08 +0100

    [YOUJ-14] Créer un endpoint REST pour la connexion

--- src/main/java/com/youjob/youjob/web/rest/auth/AuthController.java
+++ src/main/java/com/youjob/youjob/web/rest/auth/AuthController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/auth")
 public class AuthController {
-
     private final LoginMapper loginMapper;
     private final RegisterMapper registerMapper;
     private final AuthService authService;
@@ -30,7 +29,6 @@ public class AuthController {
         this.authService=authService;
         this.jwtUtil=jwtUtil;
     }
-
     @PostMapping("/register")
     public ResponseEntity<String> Register(@RequestBody @Valid RegisterVM registerVM) {
         User user=registerMapper.mapToSpecificUser(registerVM);
@@ -43,6 +41,7 @@ public class AuthController {
             throw new BadCredentialsException(e.getMessage());
         }
     }
+
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody @Valid LoginVM loginVM){
         User user=loginMapper.toUser(loginVM);
